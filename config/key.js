19f735d58103e88c07bf8d2988b23030df2c1416
Subhajit <subhabera211@gmail.com>
2023-08-31 22:24:49 +0530

    update

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MongoURI:
+    // " mongodb://127.0.0.1:27017/authantication",
+   "mongodb+srv://subhajit108chs:sapY2fVIHmyjj6zP@cluster0.etldepg.mongodb.net/?retryWrites=true&w=majority"
+};
